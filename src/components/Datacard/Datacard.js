fafac743a4b34ba807de7812841bf4905fe86d6f
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-09-26 11:42:28 +0600

    css

--- src/components/Datacard/Datacard.js
+++ src/components/Datacard/Datacard.js
@@ -1,8 +1,15 @@
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Datacard.css";
 
 const Datacard = (props) => {
    console.log(props.card);
+   const bagIcon = <FontAwesomeIcon icon={faShoppingBag} />;
+
+   const facebookIcon = <i class="fab fa-facebook-square"></i>;
+   const twitterICon = <i class="fab fa-twitter-square"></i>;
+
    const { name, pic, age, income, role } = props.card;
 
    return (
@@ -19,12 +26,26 @@ const Datacard = (props) => {
             className="img-fluid"
             alt=""
          />
-         <p>Actor: {name}</p>
-         <p>Character: {role}</p>
-         <p>Age: {age}</p>
          <p>
-            Salary: €{income} <small>per episode</small>
+            Actor: <b>{name}</b>
          </p>
+         <p>
+            Character: <b>{role}</b>
+         </p>
+         <p>
+            Age: <b>{age}</b>
+         </p>
+         <p>
+            Salary: €{income}{" "}
+            <small>
+               {" "}
+               <b>per episode</b>
+            </small>
+         </p>
+         <p>
+            {facebookIcon} {twitterICon}
+         </p>
+         <button className="btn btn-success"> {bagIcon} Add to list</button>
       </div>
    );
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -13,7 +13,7 @@ const Main = () => {
    }, []);
 
    return (
-      <div className="section">
+      <div className="section container-fluid">
          {/* card container  */}
          <div className="card_container">
             {cards.map((card) => (
